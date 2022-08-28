8f5391a541f49ce2d9396876cf394b60ab8b3d65
Gerseneck <wang14240@gmail.com>
2022-08-28 10:19:25 -0400

    maybe refactoring

--- main.py
+++ main.py
@@ -173,25 +173,24 @@ def edit_mode(words) -> None:
     if mode in {'e', 'edit'}:
         while True:
             edit_num = input('Enter the number you want to edit (Blank to quit): ')
-            if edit_num == '':
+            if not edit_num:
                 break
             edit_term = input('Enter the new term (Blank to keep term): ')
             edit_def = input('Enter the new definition (Blank to keep definition): ')
-            if edit_term != '':
+            if edit_term:
                 words[int(edit_num)].word = edit_term
-            if edit_def != '':
+            if edit_def:
                 words[int(edit_num)].definition = edit_def
             print()
         print(CLEAR)
     elif mode in {'+', 'add'}:
         while True:
             term = input('Enter a term: ')
-            if term == '':
-                break
             definition = input('Enter a definition: ')
-            if definition == '':
+            if not (term and definition):
+                print('Finishing!')
                 break
-            words.append(utils.KeyData(word=term, definition=definition, last_covered=-1, repetition_spot=0))
+            words.append(utils.KeyData(term, definition, -1, 0))
             print()
         print(CLEAR)
     elif mode in {'-', 'remove'}:
@@ -200,10 +199,10 @@ def edit_mode(words) -> None:
                 print(f'{CLEAR}{C.yellow}You may not remove a set with 1 term.{C.end}')
                 break
             remove_num = input('Enter the number you want to remove (Blank to quit): ')
-            if remove_num == '':
+            if not remove_num:
                 break
             remove_confirm = input(f'Are you sure you want to remove term {C.bwhite}{words[int(remove_num)].word}{C.end} [Y/n]. ')
-            if remove_confirm != 'y':
+            if remove_confirm.lower() != 'y':
                 print('Aborted!')
                 break
             del words[int(remove_num)]
