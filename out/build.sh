f080e96768b9b0bcccabca46b4b151e8ea3163f5
greatericontop <greateric@protonmail.com>
2022-09-10 16:03:21 -0400

    fix build script

--- out/build.sh
+++ out/build.sh
@@ -1,8 +1,12 @@
-\# Run me to compile to a binary using PyInstaller.
+# Run me to compile to a binary using PyInstaller.
 # The output file should be in the 'dist' directory.
 
 # NOTES:
 # - Weakly encrypt/obfuscate the code to hopefully prevent antivirus falses. (GreatStudier is still GPL3)
 # - Both `rapidfuzz` and its dependency `jarowinkler` need to be all-submodule-imported.
 
-pyinstaller --onefile --clean --key GREATSTUDIER ../main.py
+if [ "$1" == "clean" ]; then
+  pyinstaller --onefile --key GREATSTUDIER --clean ../main.py
+else
+  pyinstaller --onefile --key GREATSTUDIER ../main.py
+fi
