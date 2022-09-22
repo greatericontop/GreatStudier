b09a8229584ccff1e97b659cecf5a9fb56cc151c
greatericontop <greateric@protonmail.com>
2022-09-21 20:47:42 -0400

    dont crash when we try to rename to something blank

--- set_manager.py
+++ set_manager.py
@@ -125,6 +125,8 @@ def edit_mode(words) -> None:
         new_name = input('Enter a new name for the set: ')
         for c in ILLEGAL_FILENAME_CHARS:
             new_name.replace(c, '_')
+        if not new_name:
+            print('Nothing was provided!')
         old_set_path = config.get_set_directory() / config.config['set']
         new_set_path = config.get_set_directory() / new_name
         old_set_path.rename(new_set_path)
